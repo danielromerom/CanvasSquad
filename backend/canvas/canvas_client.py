2c22b6964054117b5308a8f27c0df4cba415862c
radliff <radliffjeantinor@gmail.com>
2026-02-10 21:39:06 -0500

    minor fixes

--- backend/canvas/canvas_client.py
+++ backend/canvas/canvas_client.py
@@ -16,24 +16,27 @@ class CanvasClient:
         return r.json()
 
     def list_courses(self):
-    url = urljoin(self.base_url, "/api/v1/courses")
-    r = self.session.get(
-        url,
-        params={
-            "per_page": 100,
-            "enrollment_state": "active"
-        }
-    )
-    r.raise_for_status()
-    return r.json()
+        url = urljoin(self.base_url, "/api/v1/courses")
+        r = self.session.get(
+            url,
+            params={
+                "per_page": 100,
+                "enrollment_state": "active"
+            }
+        )
+        r.raise_for_status()
+        return r.json()
 
     def list_modules(self, course_id):
-    url = urljoin(self.base_url, f"/api/v1/courses/{course_id}/modules")
-    r = self.session.get(
-        url,
-        params={
-            "per_page": 100
-        }
-    )
-    r.raise_for_status()
-    return r.json()
+        url = urljoin(self.base_url, f"/api/v1/courses/{course_id}/modules")
+        r = self.session.get(
+            url,
+            params={
+                "per_page": 100
+            }
+        )
+        r.raise_for_status()
+        return r.json()
+
+''' http://127.0.0.1:8000/api/canvas/courses/255/assignments/ is the endpoint we use to get assignments for a course,
+it calls the list_assignments method in this client to fetch the data from canvas.'''
